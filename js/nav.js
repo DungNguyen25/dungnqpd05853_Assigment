755da1623f8210b9212c6730d6adf92b5a23b9a7
Dung Nguyen <nqd251003@gmail.com>
2022-06-12 07:10:58 +0700

    master

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,11 @@
 function nav() {
   var x = document.getElementById("myTopnav");
-
+  const mode = document.getElementById("mode");
   if (x.className === "header-nav") {
     x.className += "--responsive";
+    mode.className += "--hide";
   } else {
     x.className = "header-nav";
+    mode.className = "toggle";
   }
 }
